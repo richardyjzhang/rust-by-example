89b3dc8f551a93e3b732745d790f8dfec2596bd9
ZhangYJ <richard.yujia.zhang@gmail.com>
2022-07-18 21:49:18 +0800

    Comments

---
+++ 01HelloWorld/comments.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // This is an example of a line comment
+
+    // println!("Hello World!");
+    println!("Hello World!");
+    
+    /*
+     * This is another type of comment
+     * Block comments can be /* nested,  */ 
+     The previous '*' was entirely for style
+     There's no actural need for it
+     */
+     let x = 5 + /* 90 + */ 5;
+     println!("Is x 10 or 100? x = {}", x);
+}
