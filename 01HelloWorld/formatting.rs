8b0d737e35dc4ab9605b0245765c4b8b0a840f65
ZhangYJ <richard.yujia.zhang@gmail.com>
2022-07-19 20:13:24 +0800

    formatting

---
+++ 01HelloWorld/formatting.rs
@@ -0,0 +1,95 @@
+use std::fmt::{self, Display, Formatter};
+
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl Display for City {
+    // `f` is a buffer,
+    // and this method must write the formatted string into it
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        // `write!` is like `format!` but it will write the
+        // formatted string into a buffer
+        write!(
+            f,
+            "{}: {:.3}° {} {:.3}° {}",
+            self.name,
+            self.lat.abs(),
+            lat_c,
+            self.lon.abs(),
+            lon_c
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        let r = format!("{:X}", self.red);
+        let g = format!("{:X}", self.green);
+        let b = format!("{:X}", self.red);
+
+        write!(
+            f,
+            "RGB ({}, {}, {}) 0x{:0>2}{:0>2}{:0>2}",
+            self.red, self.green, self.blue, r, g, b
+        )
+    }
+}
+
+fn main() {
+    for city in [
+        City {
+            name: "Dublin",
+            lat: 53.347778,
+            lon: -6.259722,
+        },
+        City {
+            name: "Oslo",
+            lat: 59.95,
+            lon: 10.75,
+        },
+        City {
+            name: "Vancouver",
+            lat: 49.25,
+            lon: -123.1,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *city);
+    }
+
+    for color in [
+        Color {
+            red: 128,
+            green: 255,
+            blue: 90,
+        },
+        Color {
+            red: 0,
+            green: 3,
+            blue: 254,
+        },
+        Color {
+            red: 0,
+            green: 0,
+            blue: 0,
+        },
+    ]
+    .iter()
+    {
+        println!("{}", *color);
+    }
+}
